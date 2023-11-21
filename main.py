7f1d1ec744225cabdb39597f12e7ea2b6e8a8873
Joakim Lefebvre <joakim.lefebvre23@gmail.com>
2023-11-21 15:34:14 +0100

    debut of quatric function

--- main.py
+++ main.py
@@ -10,23 +10,32 @@ def roots_third_degree(a, b, c, d):
 
 
 def init_matrix():
-    A = np.array([[4, 5, 2, 6],
+    M = np.array([[4, 5, 2, 6],
                   [9, 4, 5, 6],
                   [4, 3, 6, 9],
                   [10, 1, 6, 4],
                   [1, 7, 9, 11]])
     r = 2
-    U = np.random.rand(A.shape[0], r)
-    V = np.random.rand(r, A.shape[1])
-    return A, U, V
+    U = np.random.rand(M.shape[0], r)
+    V = np.random.rand(r, M.shape[1])
+    return M, U, V
+
+
+def quartic_function(M, U, V):
+    # On calcule pour la variable x1
+    m, n = M.shape
+    a = 0
+    b = 0
+    for i in range(m):
+        a += np.power(U[i][0], 4)
+    for i in range(m):
+        b += np.power(U[i][0], 3)*U[i][1]*V[1][0]
 
 
 def main():
-    roots = roots_third_degree(4, -3, 0, 1)
-    print("Les racines du polynomes sont:", roots)
-    A, U, V = init_matrix()
-    print("Matrice A :")
-    for row in A:
+    M, U, V = init_matrix()
+    print("Matrice M :")
+    for row in M:
         print(row)
     print("Matrice U initiale :")
     for row in U:
@@ -34,6 +43,7 @@ def main():
     print("Matrice V initiale :")
     for row in V:
         print(row)
+    quartic_function(M, U, V)
 
 
 if __name__ == "__main__":
