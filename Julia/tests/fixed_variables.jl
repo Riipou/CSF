41adb9476772e9b115d85849344870c67b13e429
Joakim Lefebvre <joakim.lefebvre23@gmail.com>
2024-05-18 14:55:37 +0200

    exact factorization test

--- Julia/tests/fixed_variables.jl
+++ Julia/tests/fixed_variables.jl
@@ -5,10 +5,10 @@ include("../algorithms/slackgon.jl")
 using MAT
 using Random
 
-function fixed_slackngon(U, V, indices_V, indices_U)
+function fixed_slackngon(U, V,indices_U, indices_V)
     m, _ = size(U)
     M, _ = slackngon(m)
-    U,V = coordinate_descent(1000000, M, U, V, indices_V = indices_V, indices_U = indices_U)
+    U,V = coordinate_descent(1000000, M, U, V)
     println(norm(M - (U * V).^2) / norm(M))
     # Create a dictionary containing the matrices
     data_dict = Dict("U" => U, "V" => V)
@@ -84,13 +84,13 @@ n = 8
 r = 6
 
 M, _ = slackngon(n)
-file_name = "8x8_r=6_best_error=6.902703883761576e-5"
+file_name = "slackngon_8x8_r=6_error=4.175785360688005e-8"
 # Path of initial U and V
 file_path = "results/fixed_variables/$(file_name).mat"
 # Load the .mat file
 data = matread(file_path)
-U = data["bU"]
-V = data["bV"]
+U = data["U"]
+V = data["V"]
 println("Matrix U:")
 # Print each row of U row by row
 for i in 1:size(U)[1]
@@ -104,16 +104,9 @@ end
 
 Xt = sign.(U*V).*sqrt(M)
 println(rank(Xt))
-
-#= U[1,1] = 0.04999974695811232
-U[5,4] = 0.19
-V[1,1] = 2
-V[1,3] = 1.78
-V[2,6] = -2.28
-V[3,6] = 2
-V[3,1] = 1.7
-indices_U = [(1,1), (5,4)]
-indices_V = [(1,1), (1,3), (2,6), (3,1), (3,6)]
-fixed_slackngon(U,V, indices_V, indices_U) =#
-
+indices_U = [(2,4)]
+indices_V = [(5,2)]
+for _ in 1:100
+    fixed_slackngon(U,V,indices_U, indices_V)
+end
 #= fixed_slackgon_matrix(n,r) =#
